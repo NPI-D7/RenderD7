bfa3b0a1c6eb1ac8e2c4ebf25949d102d0612023
Tobi <tobid7@outlook.de>
2021-07-24 23:59:29 +0200

    Update sound.hpp

--- sound.hpp
+++ sound.hpp
@@ -3,11 +3,21 @@
 #include <3ds.h>
 #include <string>
 
+/** Sound Class */
 class sound {
 public:
+        /**
+        *Construct new Soundeffect
+        *path: Path to the .wav file
+        *channel: the channel 1-23
+        *toloop: true:loop the sound, false: don't loop
+        "/
 	sound(const std::string& path, int channel = 1, bool toloop = false);
+        /** deconstruct the sound */
 	~sound();
+        /** play the sound */
 	void play();
+        /** stop the sound */
 	void stop();
 
 private:
