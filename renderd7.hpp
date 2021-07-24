f0b02d64ef4629e4d833e63b5295a68a8c7310bc
Tobi <tobid7@outlook.de>
2021-07-24 18:16:51 +0200

    Update renderd7.hpp

--- renderd7.hpp
+++ renderd7.hpp
@@ -39,6 +39,9 @@ extern std::string dspststus;
 
 namespace RenderD7
 {
+   /*	Set current RenderScreen
+   *	target: The RenderTarget Top, Bottom
+   */
     void OnScreen(C3D_RenderTarget *target);
     class Sheet
     {
