c733394277be22e37fad7ec373bb8b7d82c5e000
Tobi <tobid7@outlook.de>
2021-07-25 18:34:47 +0200

    Update log.hpp

--- log.hpp
+++ log.hpp
@@ -17,13 +17,14 @@ class Log
                 /// \param filename name for the file
 		void Init(const char *filename);
                 /// Write Text to logfile
-                /// debug_text: your text
+                /// \param debug_text your text
 		void Write(std::string debug_text);
                 /// Get the date
 		std::string logDate(void);
                 /// Format to logstyle
-                /// fmt_str: the formatted style
+                /// \param fmt_str the formatted style
 		std::string format(const std::string& fmt_str, ...);
 	private:
+                /// param filename the name of the logfile
 		std::string filename;
 };
