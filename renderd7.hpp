ed2b2c4ed869571421b55b43ec7bab24ec51ada6
Tobi <tobid7@outlook.de>
2021-07-25 16:30:35 +0200

    Update renderd7.hpp

--- renderd7.hpp
+++ renderd7.hpp
@@ -72,9 +72,15 @@ namespace RenderD7
         /// Draw the Image directly
         /// \param x The x position
         /// \param y the y position
+        /// @param scaleX x scale from 0.0 to 1.0
+        /// @param scaleY y scale from 0.0 to 1.0
         bool Draw(float x, float y, float scaleX = 1.0f, float scaleY = 1.0f);
+        /// @brief Get The Image
+        /// @return C2D_Image
         C2D_Image Get(){return this->img;}
+        /// @img this is the C2D_Image
         C2D_Image img;
+        /// @loadet whether the image is loadet or not
         bool loadet = false;
     };
     /// Sprite Class
