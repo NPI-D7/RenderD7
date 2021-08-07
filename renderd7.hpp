0d4cc6f30ff8db2f4e2547eb80e27acffbf5465c
Tobi <tobid7@outlook.de>
2021-08-07 17:07:57 +0200

    Update renderd7.hpp

--- renderd7.hpp
+++ renderd7.hpp
@@ -40,6 +40,15 @@ extern std::string dspststus;
 /// RenderD7
 namespace RenderD7
 {
+    enum kbd{
+         SWKBD,
+         BKBD
+    };
+    enum kbd_type
+    {
+         NUMPAD,
+         STANDARD
+    }
     /// Set current RenderScreen
     /// \param target The RenderTarget Top, Bottom
     void OnScreen(C3D_RenderTarget *target);
