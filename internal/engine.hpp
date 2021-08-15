13916e3dff01292e32a4e7825b5e3c1c97291755
Tobi <tobid7@outlook.de>
2021-08-15 22:47:48 +0200

    Update engine.hpp

--- internal/engine.hpp
+++ internal/engine.hpp
@@ -1,7 +1,25 @@
 #pragma once
 #include <3ds.h>
 #include <citro2d.h>
-#imclude <citro3d.h>
+#include <citro3d.h>
+#include <memory>
+#include <stack>
+#include <string>
+#include <functional>
+#include <vector>
+#include <dirent.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <cstring>
+#include <sys/stat.h>
+#include <algorithm>
+#include <iostream>
+#include <codecvt>
+#include "lang.hpp"
+#include "parameter.hpp"
+#include "thread.hpp"
+#include "ini.hpp"
+#include "stringtool.hpp"
 
 enum EngineType
 {
