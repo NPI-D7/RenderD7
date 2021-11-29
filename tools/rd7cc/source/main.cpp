61285be23e54e9d5e76222df260d60f282d9f28b
Tobi <tobid7@outlook.de>
2021-11-29 12:10:36 +0100

    Update main.cpp

--- tools/rd7cc/source/main.cpp
+++ tools/rd7cc/source/main.cpp
@@ -1,7 +1,11 @@
 //rd7cc
 #include <iostream>
-
+#include <fstream>
 int main(int argc, char* argv[])
 {
-    
+    std::ofstream result ("result.hpp");
+
+    result << "//Result" << std::endl;
+
+    result.close();
 }
