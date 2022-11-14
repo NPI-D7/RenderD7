62fa5ede9d1bb1c1752c3210647d12cc0740f0d5
Tobi <tobid7@outlook.de>
2022-11-14 16:27:04 +0100

    Standarts of LLVM

--- include/renderd7/StealConsole.hpp
+++ include/renderd7/StealConsole.hpp
@@ -1,16 +1,16 @@
 #pragma once
-#include <string>
 #include <sstream>
+#include <string>
 
-namespace RenderD7
-{
-    class StealConsole
-    {
-        public:
-        StealConsole();
-        ~StealConsole();
-        std::string GetStdout();
-        private:
-        std::stringstream stolen_stdout;
-    };
-}
+
+namespace RenderD7 {
+class StealConsole {
+public:
+  StealConsole();
+  ~StealConsole();
+  std::string GetStdout();
+
+private:
+  std::stringstream stolen_stdout;
+};
+} // namespace RenderD7
